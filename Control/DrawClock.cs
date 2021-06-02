6bc52557f600ab919b643176c7b709dd3f415c3b
manlaan <73758520+manlaan@users.noreply.github.com>
2021-06-02 12:30:00 -0600

    .

---
+++ Control/DrawClock.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Blish_HUD;
+using Blish_HUD.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.BitmapFonts;
+
+namespace Clock
+{
+    class DrawClock : Container
+    {
+        public bool ShowLocal = false;
+        public bool ShowTyria = false;
+        public bool ShowServer = false;
+        public ContentService.FontSize Font_Size;
+        public DateTime LocalTime;
+        public DateTime TyriaTime;
+        public DateTime ServerTime;
+
+        private static BitmapFont _font; 
+
+        public DrawClock()
+        {
+            this.ShowLocal = false;
+            this.ShowTyria = false;
+            this.ShowServer = false;
+            this.Font_Size = ContentService.FontSize.Size11;
+            this.LocalTime = DateTime.Now;
+            this.TyriaTime = DateTime.Now;
+            this.ServerTime = DateTime.Now;
+            this.Location = new Point(0, 0);
+
+            this.Size = new Point(0, 0);
+            this.Visible = true;
+            this.ZIndex = 0;
+            this.Padding = Thickness.Zero;
+        }
+
+        protected override CaptureType CapturesInput()
+        {
+            return CaptureType.ForceNone;
+        }
+
+
+        public override void PaintBeforeChildren(SpriteBatch spriteBatch, Rectangle bounds)
+        {
+            string text = "";
+            int height;
+            int width;
+            _font = GameService.Content.GetFont(ContentService.FontFace.Menomonia, Font_Size, ContentService.FontStyle.Regular);
+
+            if (this.ShowLocal)
+                text += "Local: " + LocalTime.ToString("h:mm tt") + "\n";
+            if (this.ShowTyria)
+                text += "Tyria: " + TyriaTime.ToString("h:mm tt") + "\n";
+            if (this.ShowServer)
+                text += "Server: " + ServerTime.ToString("h:mm tt") + "\n";
+            width = (int)_font.MeasureString(text).Width;
+            height = (int)_font.MeasureString(text).Height;
+            this.Size = new Point(width, height);
+
+            spriteBatch.DrawStringOnCtrl(this, 
+                text, 
+                _font,
+                new Rectangle(0, 0, width, height), 
+                Color.White, 
+                false, 
+                true, 
+                1, 
+                HorizontalAlignment.Right, 
+                VerticalAlignment.Top
+                );
+
+            //rect = new Rectangle((Size.X / 2) - (width / 2) - 1, rect.Y + height + 2, width + 2, _underlineSize + 2);
+            //spriteBatch.DrawOnCtrl(this, ContentService.Textures.Pixel, rect, new Color(0, 0, 0, 200));
+            //rect = new Rectangle(rect.X + 1, rect.Y + 1, width, _underlineSize);
+            //spriteBatch.DrawOnCtrl(this, ContentService.Textures.Pixel, rect, _brightGold);
+
+        }
+
+    }
+}
---
+++ Module.cs
@@ -0,0 +1,132 @@
+﻿using Blish_HUD;
+using Blish_HUD.Modules;
+using Blish_HUD.Modules.Managers;
+using Blish_HUD.Settings;
+using Microsoft.Xna.Framework;
+using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    [Export(typeof(Blish_HUD.Modules.Module))]
+    public class Module : Blish_HUD.Modules.Module
+    {
+
+        private static readonly Logger Logger = Logger.GetLogger<Module>();
+
+        #region Service Managers
+        internal SettingsManager SettingsManager => this.ModuleParameters.SettingsManager;
+        internal ContentsManager ContentsManager => this.ModuleParameters.ContentsManager;
+        internal DirectoriesManager DirectoriesManager => this.ModuleParameters.DirectoriesManager;
+        internal Gw2ApiManager Gw2ApiManager => this.ModuleParameters.Gw2ApiManager;
+        #endregion
+
+        private SettingEntry<bool> _settingClockLocal;
+        private SettingEntry<bool> _settingClockTyria;
+        private SettingEntry<bool> _settingClockServer;
+        private SettingEntry<ContentService.FontSize> _settingClockFontSize;
+        private SettingEntry<string> _settingClockX;
+        private SettingEntry<string> _settingClockY;
+        private DrawClock _clockImg;
+
+        [ImportingConstructor]
+        public Module([Import("ModuleParameters")] ModuleParameters moduleParameters) : base(moduleParameters) { }
+
+        protected override void DefineSettings(SettingCollection settings)
+        {
+            
+            _settingClockLocal = settings.DefineSetting("ClockLocal", true, "Local", "");
+            _settingClockTyria = settings.DefineSetting("ClockTyria", true, "Tyria", "");
+            _settingClockServer = settings.DefineSetting("ClockServer", false, "Server", "");
+            _settingClockFontSize = settings.DefineSetting("ClockFont", ContentService.FontSize.Size12, "Font Size", "");
+            _settingClockX = settings.DefineSetting("ClockX", "100", "X", "");
+            _settingClockY = settings.DefineSetting("ClockY", "100", "Y", "");
+            //_settingFontSize.SetRange(1, 20);
+        }
+
+        protected override void Initialize()
+        {
+            _settingClockLocal.SettingChanged += UpdateClockSettings_Show;
+            _settingClockTyria.SettingChanged += UpdateClockSettings_Show;
+            _settingClockServer.SettingChanged += UpdateClockSettings_Show;
+            _settingClockFontSize.SettingChanged += UpdateClockSettings_Font;
+            _settingClockX.SettingChanged += UpdateClockSettings_Location;
+            _settingClockY.SettingChanged += UpdateClockSettings_Location;
+
+            _clockImg = new DrawClock();
+            _clockImg.Parent = GameService.Graphics.SpriteScreen;
+            UpdateClockSettings_Show();
+            UpdateClockSettings_Font();
+            UpdateClockSettings_Location();
+        }
+
+        protected override async Task LoadAsync()
+        {
+
+        }
+
+        protected override void OnModuleLoaded(EventArgs e)
+        {
+            base.OnModuleLoaded(e);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            _clockImg.LocalTime = DateTime.Now;
+            _clockImg.TyriaTime = CalcTyriaTime();
+            _clockImg.ServerTime = CalcServerTime();
+        }
+
+        /// <inheritdoc />
+        protected override void Unload()
+        {
+            _settingClockLocal.SettingChanged -= UpdateClockSettings_Show;
+            _settingClockTyria.SettingChanged -= UpdateClockSettings_Show;
+            _settingClockServer.SettingChanged -= UpdateClockSettings_Show;
+            _settingClockFontSize.SettingChanged -= UpdateClockSettings_Font;
+            _settingClockX.SettingChanged -= UpdateClockSettings_Location;
+            _settingClockY.SettingChanged -= UpdateClockSettings_Location;
+            _clockImg?.Dispose();
+        }
+
+        private void UpdateClockSettings_Show(object sender = null, ValueChangedEventArgs<bool> e = null)
+        {
+            _clockImg.ShowLocal = _settingClockLocal.Value;
+            _clockImg.ShowTyria = _settingClockTyria.Value;
+            _clockImg.ShowServer = _settingClockServer.Value;
+        }
+        private void UpdateClockSettings_Font(object sender = null, ValueChangedEventArgs<ContentService.FontSize> e = null)
+        {
+            _clockImg.Font_Size = _settingClockFontSize.Value;
+        }
+        private void UpdateClockSettings_Location(object sender = null, ValueChangedEventArgs<string> e = null)
+        {
+            if (int.Parse(_settingClockX.Value) <= 0)
+                _settingClockX.Value = "1";
+            if (int.Parse(_settingClockY.Value) <= 0)
+                _settingClockY.Value = "1";
+
+            _clockImg.Location = new Point(int.Parse(_settingClockX.Value), int.Parse(_settingClockY.Value));
+        }
+        private DateTime CalcServerTime()
+        {
+            
+            return DateTime.UtcNow;
+        }
+        private DateTime CalcTyriaTime()
+        {
+            DateTime UTC = DateTime.UtcNow;
+            int utcsec = utcsec = (UTC.Hour * 3600) + (UTC.Minute * 60) + UTC.Second; 
+            int tyriasec = (utcsec *  12)-60;
+            tyriasec = tyriasec % (3600*24);
+            int tyrianhour = (int)(tyriasec / 3600);
+            tyriasec = tyriasec % 3600;
+            int tyrianmin = (int)(tyriasec / 60);
+            tyriasec = tyriasec % 60;
+            return new DateTime (2000,1,1,tyrianhour,tyrianmin,tyriasec);
+        }
+
+    }
+
+}
